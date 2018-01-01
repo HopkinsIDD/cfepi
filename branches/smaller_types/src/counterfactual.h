a00f5b87f63f5bcaf33adfc8d0909325d148997a
jkaminsky@jhu.edu <jkaminsky@jhu.edu@98181e47-6001-0010-825e-436e3f03b174>
2018-01-01 20:09:43 +0000

    Starting to set things up for interfacing with R well.  Making a framework for passing a standardized function to the constructTimeSeries function, without sacrificing any flexibility a priori.  Its segfaulting right now though

--- branches/smaller_types/src/counterfactual.h
+++ branches/smaller_types/src/counterfactual.h
@@ -1,26 +1,21 @@
 #ifndef COUNTERFACTUAL_H_
 #define COUNTERFACTUAL_H_
 
-#include <stdint.h>
+#include "types.h"
+#include "twofunctions.h"
+
 #define IND(i,j,n) ((i) * (n) + (j))
 #define IND4(i,j,k,l,n,o,p) ((i) * (n) * (o) * (p) + (j) * (o) * (p) + (k) * (p) + (l))
 #define IND5(i,j,k,l,m,n,o,p,q) ((i) * (n) * (o) * (p) * (q) + (j) * (o) * (p) *(q) + (k) * (p) * (q) + (l) * (q) + (m))
 #define MAX(x,y) (x) > (y) ? (x) : (y);
 #define MIN(x,y) (x) < (y) ? (x) : (y);
 
-// Custom types for storage efficiency
-# define bool_t uint8_t
-# define var_t uint8_t
-# define person_t uint32_t
-# define step_t uint16_t
 
 void runCounterfactualAnalysis(char*,person_t*,var_t,step_t, double*, double*,char*,char*);
 void runFastCounterfactualAnalysis(person_t*,var_t,step_t, double*, double*,char*,char*);
-void constructTimeSeries(person_t* ,var_t, step_t,bool_t (*reduceBeta)(step_t,person_t,person_t,var_t,var_t),void (*eliminateSusceptible)(var_t**,step_t,step_t,person_t),char*,char*,char*);
+void constructTimeSeries(person_t* ,var_t, step_t,saved_beta_t reduceBeta,saved_susceptible_t eliminateSusceptible,char*,char*,char*);
 //Fake R functions
 void sample(person_t**, person_t,person_t);
 void swap(person_t*,person_t*);
-void no_interventionSusceptible(var_t**,step_t,step_t,person_t);
-bool_t no_interventionBeta(step_t,person_t,person_t,var_t,var_t);
 
 #endif //COUNTERFACTUAL_H_
---
+++ branches/smaller_types/src/twofunctions.h
@@ -0,0 +1,48 @@
+#ifndef INTERVENTION_H_
+#define INTERVENTION_H_
+
+#include "types.h"
+
+typedef struct{
+  int time;
+} param_beta_t;
+
+typedef bool_t (*beta_t)(step_t, person_t, person_t, var_t, var_t, param_beta_t);
+
+typedef struct {
+  beta_t func;
+  param_beta_t pars;
+} saved_beta_t;
+
+saved_beta_t partially_evaluate_beta(beta_t, param_beta_t);
+bool_t invoke_beta_t(saved_beta_t,step_t,person_t,person_t,var_t,var_t);
+
+bool_t interventionBeta(step_t,person_t,person_t,var_t,var_t,param_beta_t);
+bool_t no_interventionBeta(step_t,person_t,person_t,var_t,var_t,param_beta_t);
+
+typedef struct{
+  int time;
+} param_susceptible_t;
+
+typedef void (*susceptible_t)(var_t**, step_t, step_t, person_t,param_susceptible_t);
+
+typedef struct {
+  susceptible_t func;
+  param_susceptible_t pars;
+} saved_susceptible_t;
+
+saved_susceptible_t partially_evaluate_susceptible(susceptible_t, param_susceptible_t);
+void invoke_susceptible_t(saved_susceptible_t,var_t**,step_t,step_t,person_t);
+
+void no_interventionSusceptible(var_t**,step_t,step_t,person_t,param_susceptible_t);
+void interventionSusceptible(var_t**,step_t,step_t,person_t,param_susceptible_t);
+/*
+void parametrized_interventionSusceptible(var_t**,step_t,step_t,person_t, param_beta_t);
+bool_t parametrized_intervenionBeta(step_t,person_t,person_t,var_t,var_t, param_beta_t);
+
+void no_interventionSusceptible(var_t**,step_t,step_t,person_t);
+bool_t no_interventionBeta(step_t,person_t,person_t,var_t,var_t);
+*/
+
+#endif //INTERVENTION_H_
+
