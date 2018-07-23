ac0807445b39cbf83fee0f4dbefa7f7321ce546c
jkaminsky@jhu.edu <jkaminsky@jhu.edu@98181e47-6001-0010-825e-436e3f03b174>
2018-07-23 12:35:28 +0000

    Created a branch for doing network models, since its so easy.

---
+++ branches/network/src/single_intervention.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "single_intervention.h"
+
+#include <R.h>
+#include <R_ext/Print.h>
+#include <Rmath.h>
+param_beta_t param_single_beta(){
+  param_beta_t rc;
+  strcpy(rc.type , "single");
+  rc.data = malloc(1 * sizeof(data_beta_single_t));
+  return(rc);
+};
+
+// 1 success 0 failure
+bool_t free_param_single_beta(param_beta_t rc){
+  if(strcmp(rc.type,"single")!=0){
+    Rf_error("Attempting to free a param_beta_t with the wrong destructor\n");
+    return(1);
+  }
+  free(rc.data);
+  return(0);
+};
+
+// Finish writing these
+bool_t single_beta(step_t time, person_t person1, person_t person2, var_t var1, var_t var2,param_beta_t pars){
+  return(1);
+}
+
+param_susceptible_t param_single_susceptible(step_t time, float rate){
+  param_susceptible_t rc;
+  strcpy(rc.type,"single");
+  rc.data = malloc(1 * sizeof(data_susceptible_single_t));
+  (* ( (data_susceptible_single_t *) rc.data) ).time = time;
+  (* ( (data_susceptible_single_t *) rc.data) ).rate = rate;
+  return(rc);
+};
+
+bool_t free_param_single_susceptible(param_susceptible_t rc){
+  if(strcmp(rc.type,"single")!=0){
+    Rf_error("Attempting to free a param_susceptible_t with the wrong destructor\n");
+    return(1);
+  }
+  free(rc.data);
+  return(0);
+};
+
+// Finish writing these
+void single_susceptible(var_t** states, step_t time, step_t ntime, person_t npop, param_susceptible_t pars){
+  return;
+}
+
---
+++ branches/network/src/types.h
@@ -0,0 +1,12 @@
+#ifndef COUNTERFACTUAL_TYPES_H_
+#define COUNTERFACTUAL_TYPES_H_
+
+#include <stdint.h>
+// Custom types for storage efficiency
+# define bool_t uint8_t
+# define var_t uint8_t
+# define person_t uint32_t
+# define step_t uint16_t
+# define NODE_NAME_LEN 20
+
+#endif
