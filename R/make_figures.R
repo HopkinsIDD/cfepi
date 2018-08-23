57aca0aec23ab0fc3c6dbe9e9eceb81348b7faa0
jkaminsky <20066235+jkamins7@users.noreply.github.com>
2018-08-23 14:21:32 +0000

    Updated figure making script

--- R/make_figures.R
+++ R/make_figures.R
@@ -1,10 +1,12 @@
-try({remove.packages('counterfactual')},silent=T)
-install.packages('package',type='source',repos=NULL)
+# try({remove.packages('counterfactual')},silent=T)
+# install.packages('package',type='source',repos=NULL)
 library(counterfactual)
 library(ggplot2)
 library(dplyr)
+library(tidyr)
 
-output = read_scenario('output/figures')
+output = read_scenario('output/figures0')
+multiworld_output = read_scenario('output/figures1')
 
 plt = ggplot(output) + geom_point(aes(x=t,y=V1,color=paste(beta_name,susceptible_name)))
 print(plt)
@@ -20,24 +22,39 @@ dev.off()
 #### Final Size
 plt2 <- output %>%
   filter(t == max(t))  %>%
+  calculate_residual() %>%
+  gather('scenario','value',None_None,None_Single,None_Constant,Flat_None) %>% 
+  spread(variable,value) %>%
   ggplot() +
-  geom_boxplot(aes(x = paste(beta_name,susceptible_name),y=V3))
+  geom_boxplot(aes(x = scenario,y=V3))
 
 #### Peak Estimation
-plt3 <- output %>%
-  group_by(trial,beta_name,susceptible_name) %>%
+reshaped_output <- output %>%
+  group_by(beta_name,susceptible_name,trial) %>%
   do({
     tmp = .
     tmp$max_V2 = max(.$V2)
     tmp
-  }) %>% 
-  ungroup() %>% 
+  }) %>%
+  ungroup() %>%
   filter(V2 == max_V2)  %>%
-  ggplot() +
-  geom_boxplot(aes(x = paste(beta_name,susceptible_name),y=t))
+  select(t,beta_name,susceptible_name,trial) %>% 
+  unite(scenario,beta_name,susceptible_name) %>%
+  spread(scenario,t)
 
-pdf('figures/intervention-effects.pdf')
+plt3 <- reshaped_output %>% 
+  mutate_if(
+    !colnames(reshaped_output) %in% c('trial','t'),
+    funs(as.numeric(.) - as.numeric(None_None))
+  ) %>% 
+  gather('scenario','value',None_None,None_Single,None_Constant,Flat_None) %>% 
+  ggplot() +
+  geom_boxplot(aes(x = scenario,y=value))
+
+pdf('figures/intervention-effects-final-size.pdf')
 print(plt2)
+dev.off()
+pdf('figures/intervention-effects-peak-estimation.pdf')
 print(plt3)
 dev.off()
 
@@ -51,16 +68,24 @@ dev.off()
     # x all interventions
     # y relative risk
     # color true counterfactual vs our methods
-    plt4 <- output %>%
-      filter(V2 == max(V2))  %>%
-      group_by(beta_name,susceptible_name) %>%
-      summarize(
-        relative_risk = V3/(V1 + V3)
-      ) %>% 
+    N = sum(output[1,c('V1','V2','V3','V4')],na.rm=T)
+
+    lhs = output %>% filter(t == max(t)) %>% select(trial,V3,beta_name,susceptible_name)
+    rhs = multiworld_output %>% filter(t == max(t)) %>% select(trial,V3)
+    multiworld_output = inner_join(lhs,rhs,by='trial')
+    multi_world_inference = multiworld_output %>% unite(scenario,beta_name,susceptible_name) %>% spread(scenario,V3.x) %>% mutate_if(c(F,F,T,T,T,T),funs(. / V3.y)) %>% gather('scenario','relative_risk',None_None,None_Single,None_Constant,Flat_None) %>% select(-V3.y)
+    single_world_inference = multiworld_output %>% unite(scenario,beta_name,susceptible_name) %>% spread(scenario,V3.x) %>% mutate_if(c(F,F,T,T,F,T),funs(. / None_None)) %>% mutate(None_None = None_None / None_None) %>% gather('scenario','relative_risk',None_None,None_Single,None_Constant,Flat_None) %>% select(-V3.y)
+    all_inference = inner_join(single_world_inference,multi_world_inference,by=c('scenario','trial'))
+    names(all_inference)[3:4] = c('single_world','multi_world')
+    all_inference = gather(all_inference,'type','relative_risk',single_world,multi_world)
+
+    plt4 <- all_inference %>%
       ggplot() +
-      geom_boxplot(aes(x = paste(beta_name,susceptible_name),y=relative_risk))
+      geom_boxplot(aes(x = scenario,y=relative_risk,color=type))
+
+
 
 pdf('figures/single-vs-multiple.pdf')
 print(plt4)
-dev.off()
+while(dev.off() != 1){}
 ### Table 1 - Computational Resource Thresholds + Time
