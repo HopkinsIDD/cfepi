64dea035b261968ca1678625ebb5428a0c36e40b
jkaminsky <20066235+jkamins7@users.noreply.github.com>
2018-08-24 04:54:16 +0000

    Updated testing data, as well as splitting data setup into counterfactual and intervention portions.

---
+++ R/setup_counterfactual.R
@@ -0,0 +1,36 @@
+try({remove.packages('counterfactual')},silent=T)
+install.packages('package',type='source',repos=NULL)
+library(counterfactual)
+
+
+R0 = 1.75 #
+mu = 2.25 #days
+gamma = 1/mu
+beta = R0 * gamma
+npop = 400000
+ntime = 365
+ntrial = 500
+
+trans <- matrix(0,3,3)
+inter <- matrix(0,3,3)
+trans[3,2] <- gamma
+inter[2,1] <- beta
+init <- c(npop - 10,10,0)
+inter <- inter/npop
+# This code is commented out as it takes a long time to run, and need only run a single time
+setup_counterfactual(
+  'output/figures0',
+  init,
+  inter,
+  trans,
+  ntime,
+  ntrial
+)
+setup_counterfactual(
+  'output/figures1',
+  init,
+  inter,
+  trans,
+  ntime,
+  ntrial
+)
--- R/setup_interventions.R
+++ R/setup_interventions.R
@@ -13,11 +13,17 @@ R0 = 1.75 #
 mu = 2.25 #days
 gamma = 1/mu
 beta = R0 * gamma
-warning("Using testing values for population")
 npop = 400000 #For testing only
 ntime = 365
 warning("Using testing values for number of trials")
-ntrial = 100 #1000 for final figures
+ntrial = 10 #1000 for final figures
+
+output_name1 = 'output/figures0'
+output_name2 = 'output/figures1'
+
+warning("Using testing directories")
+output_name1 = 'old_output/figures0'
+output_name2 = 'old_output/figures1'
 
 trans <- matrix(0,3,3)
 inter <- matrix(0,3,3)
@@ -26,33 +32,16 @@ inter[2,1] <- beta
 init <- c(npop - 10,10,0)
 inter <- inter/npop
 # This code is commented out as it takes a long time to run, and need only run a single time
-setup_counterfactual(
-  'output/figures0',
-  init,
-  inter,
-  trans,
-  ntime,
-  ntrial
-)
-setup_counterfactual(
-  'output/figures1',
-  init,
-  inter,
-  trans,
-  ntime,
-  ntrial
-)
 
 ## cross intervention parameters
-prop = .25 # 33% coverage
+prop = .25 # 25% adoption rate for intervention
 tstar = 1 #start on day 1
-
 
 ## none
 beta_pars <- list()
 susceptible_pars <- list()
 run_scenario(
-  'output/figures0',
+  output_name1,
   init,
   "None",
   "None",
@@ -62,7 +51,7 @@ run_scenario(
   ntrial
 )
 run_scenario(
-  'output/figures1',
+  output_name2,
   init,
   "None",
   "None",
@@ -73,15 +62,14 @@ run_scenario(
 )
 
 ## antivirals
-sick_reduction = 1.5 #When it works reduces days infected by 1.5
-effective_rate = .8
-rate = (1 - 1/sick_reduction) * prop * effective_rate
+sick_reduction = 16.8/24 #When it works reduces days infected by 1.5
+rate = (1 - 1/sick_reduction) * prop
 beta_pars <- list()
 move_to = 2
 move_from = 1
 susceptible_pars <- list(intervention_time = tstar, rate = rate, to=move_to, from=move_from)
 run_scenario(
-  'output/figures0',
+  output_name1,
   init,
   "None",
   "Constant",
@@ -91,14 +79,13 @@ run_scenario(
   ntrial
 )
 ## Vaccination
-effective_rate = .33 #Works 80% of the time
+effective_rate = .33 #Works 33% of the time
 rate = (effective_rate) * prop
 move_to = 3
 move_from = 0
-tstar = 1
 susceptible_pars <- list(intervention_time = tstar, rate = prop, to=move_to,from=move_from)
 run_scenario(
-  'output/figures0',
+  output_name1,
   init,
   "None",
   "Single",
@@ -108,12 +95,12 @@ run_scenario(
   ntrial
 )
 ## Social Distancing
-sigma = .75 # Use mask values
+sigma = .99 # Use mask values
 tstar = 0
 beta_pars <- list(start_time = tstar, rate= 1-sigma)
 susceptible_pars <- list()
 run_scenario(
-  'output/figures0',
+  output_name1,
   init,
   "Flat",
   "None",
