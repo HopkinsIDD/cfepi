3728535bb15cde6dd740461be386789b734e0435
jkaminsky@jhu.edu <jkaminsky@jhu.edu@98181e47-6001-0010-825e-436e3f03b174>
2018-08-24 12:25:41 +0000

    Updated

--- branches/manuscript/R/make_figures.R
+++ branches/manuscript/R/make_figures.R
@@ -78,7 +78,7 @@ all_world_inference <- function(fun,name){
       mutate_(.dots = setNames(paste(var_names,' - null'),var_names)) %>%
       gather_('scenario',name,var_names)
   }
-  return(all_world_inference)
+  return(all_inference)
 }
 
 confidence_intervals = function(fun,name){
@@ -102,34 +102,15 @@ plot_inference = function(fun,name){
 }
 
 #### Peak Estimation
-reshaped_output <- output %>%
-  group_by(beta_name,susceptible_name,trial) %>%
-  do({
-    tmp = .
-    tmp$max_V2 = max(.$V2)
-    tmp
-  }) %>%
-  ungroup() %>%
-  filter(V2 == max_V2)  %>%
-  select(t,beta_name,susceptible_name,trial) %>% 
-  unite(scenario,beta_name,susceptible_name) %>%
-  spread(scenario,t)
-
-plt3 <- reshaped_output %>% 
-  mutate_if(
-    !colnames(reshaped_output) %in% c('trial','t'),
-    funs(as.numeric(.) - as.numeric(None_None))
-  ) %>% 
-  gather('scenario','value',None_None,None_Single,None_Constant,Flat_None) %>% 
-  ggplot() +
-  geom_boxplot(aes(x = scenario,y=value))
-
 pdf('figures/intervention-effects-final-size.pdf')
-print(plt2)
+print(plot_inference(final_size,'Final_Size'))
 dev.off()
-pdf('figures/intervention-effects-peak-estimation.pdf')
-print(plt3)
+pdf('figures/intervention-effects-peak-time.pdf')
+print(plot_inference(peak_time,'Peak_Time'))
 dev.off()
+pdf('figures/intervention-effects-relative-risk.pdf')
+print(plot_inference(final_size,'Relative_Risk'))
+while(dev.off() != 1){}
 
 ## Figure 2 - Cartoon/Diagram illustrating method.
 ### SIR diagram
@@ -141,24 +122,4 @@ dev.off()
     # x all interventions
     # y relative risk
     # color true counterfactual vs our methods
-    N = sum(output[1,c('V1','V2','V3','V4')],na.rm=T)
-
-    lhs = output %>% filter(t == max(t)) %>% select(trial,V3,beta_name,susceptible_name)
-    rhs = multiworld_output %>% filter(t == max(t)) %>% select(trial,V3)
-    multiworld_output = inner_join(lhs,rhs,by='trial')
-    multi_world_inference = multiworld_output %>% unite(scenario,beta_name,susceptible_name) %>% spread(scenario,V3.x) %>% mutate_if(c(F,F,T,T,T,T),funs(. / V3.y)) %>% gather('scenario','relative_risk',None_None,None_Single,None_Constant,Flat_None) %>% select(-V3.y)
-    single_world_inference = multiworld_output %>% unite(scenario,beta_name,susceptible_name) %>% spread(scenario,V3.x) %>% mutate_if(c(F,F,T,T,F,T),funs(. / None_None)) %>% mutate(None_None = None_None / None_None) %>% gather('scenario','relative_risk',None_None,None_Single,None_Constant,Flat_None) %>% select(-V3.y)
-    all_inference = inner_join(single_world_inference,multi_world_inference,by=c('scenario','trial'))
-    names(all_inference)[3:4] = c('single_world','multi_world')
-    all_inference = gather(all_inference,'type','relative_risk',single_world,multi_world)
-
-    plt4 <- all_inference %>%
-      ggplot() +
-      geom_boxplot(aes(x = scenario,y=relative_risk,color=type))
-
-
-
-pdf('figures/single-vs-multiple.pdf')
-print(plt4)
-while(dev.off() != 1){}
 ### Table 1 - Computational Resource Thresholds + Time
