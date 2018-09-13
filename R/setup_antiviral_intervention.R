cbfbc6f4469146b8fc55c627b996cd1ce240c4b2
jkaminsky <20066235+jkamins7@users.noreply.github.com>
2018-09-13 22:41:38 +0000

    Updated intervention to include coverage.

--- R/setup_antiviral_intervention.R
+++ R/setup_antiviral_intervention.R
@@ -1,11 +1,14 @@
+options(error=recover)
+options(warn=1)
 source("R/intervention_params.R")
 ## antivirals
+coverage=.25
 sick_reduction = 16.8/24 #When it works reduces days infected by 1.5
 rate = (1/(mu - sick_reduction) - gamma)/(1-gamma) * prop
 beta_pars <- list()
 move_to = 2
 move_from = 1
-susceptible_pars <- list(intervention_time = tstar, rate = rate, to=move_to, from=move_from)
+susceptible_pars <- list(intervention_time = tstar, coverage = coverage, rate = rate, to=move_to, from=move_from)
 run_scenario(
   output_name1,
   init,
