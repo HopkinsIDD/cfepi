42a36fff8a36dda23c93fc6ff325c3679261943c
jkamins7 <jkaminsky@jhu.edu>
2021-02-25 09:24:52 -0500

    Most recent changes before merging test -> take_2

--- take_2/cfepi/include/sir.h
+++ take_2/cfepi/include/sir.h
@@ -1,14 +1,14 @@
-#include <iostream>       // std::cout, std::endl
-#include <thread>         // std::this_thread::sleep_for
-#include <chrono>         // std::chrono::seconds
+#include <chrono>   // std::chrono::seconds
+#include <iostream> // std::cout, std::endl
+#include <thread>   // std::this_thread::sleep_for
 // this_thread::yield example
-#include <atomic>         // std::atomic
+#include <atomic> // std::atomic
 #include <functional>
-#include <typeinfo>
 #include <mutex>
-#include <vector>
-#include <variant>
 #include <queue>
+#include <typeinfo>
+#include <variant>
+#include <vector>
 
 #include <range/v3/all.hpp>
 // #include <stxxl/vector>
@@ -20,7 +20,7 @@
  * SIR Helper Type Definitions                                                 *
  *******************************************************************************/
 
-enum epidemic_state {S, I, R, n_SIR_compartments};
+enum epidemic_state { S, I, R, n_SIR_compartments };
 constexpr size_t ncompartments = n_SIR_compartments;
 typedef float epidemic_time_t;
 typedef size_t person_t;
@@ -37,30 +37,35 @@ size_t global_event_counter = 0;
 
 struct sir_state {
   person_t population_size = 0;
-  std::vector<std::array<bool,ncompartments> > potential_states;
+  std::vector<std::array<bool, ncompartments>> potential_states;
   std::vector<bool> states_modified;
   epidemic_time_t time = -1;
   float beta = .2;
   float gamma = .1;
   std::string prefix;
   sir_state() noexcept = default;
-  sir_state(person_t population_size = 10) noexcept : population_size(population_size){
+  sir_state(person_t population_size = 10) noexcept
+      : population_size(population_size) {
     potential_states.resize(population_size);
     states_modified.resize(population_size);
   };
 };
 
-sir_state default_state(person_t popsize=10){
-  sir_state rc(popsize);
-  rc.potential_states[0][I] = true;
-  for(size_t i = 1 ; i < rc.potential_states.size(); ++i){
-    rc.potential_states[i][S] = true;
+sir_state default_state(person_t population_size = 10,
+                        person_t initial_infected = 1) {
+  sir_state rc(population_size);
+  auto base_range = ranges::views::iota(population_size * 0, population_size);
+  for (auto elem : base_range | ranges::views::take(initial_infected)) {
+    rc.potential_states[elem][I] = true;
   }
-  return(rc);
+  rc.potential_states[0][I] = true;
+  for (auto elem : base_range | ranges::views::drop(initial_infected)) {
+    rc.potential_states[elem][S] = true;
+  }
+  return (rc);
 }
 
-auto sample_sir_state = default_state();
-
+auto sample_sir_state = default_state(1, 0);
 
 /*******************************************************************************
  * SIR Event Definition                                                        *
@@ -71,14 +76,13 @@ auto sample_sir_state = default_state();
  * @description An event that transitions the model between states
  */
 
-template<size_t size>
-struct sir_event {
+template <size_t size> struct sir_event {
   epidemic_time_t time = -1;
-  std::array<person_t,size> affected_people = {};
-  std::array<std::array<bool,ncompartments>,size> preconditions = {};
-  std::array<std::optional<epidemic_state>,size> postconditions;
+  std::array<person_t, size> affected_people = {};
+  std::array<std::array<bool, ncompartments>, size> preconditions = {};
+  std::array<std::optional<epidemic_state>, size> postconditions;
   sir_event() noexcept = default;
-  sir_event(const sir_event&) = default;
+  sir_event(const sir_event &) = default;
 };
 
 struct infection_event : public sir_event<2> {
@@ -87,14 +91,16 @@ struct infection_event : public sir_event<2> {
   using sir_event<2>::preconditions;
   using sir_event<2>::postconditions;
   constexpr infection_event() noexcept {
-    preconditions = {std::array<bool,3>({true,false,false}),std::array<bool,3>({false,true,false})};
+    preconditions = {std::array<bool, 3>({true, false, false}),
+                     std::array<bool, 3>({false, true, false})};
     postconditions[0] = I;
   };
-  infection_event(const infection_event&) = default;
+  infection_event(const infection_event &) = default;
   infection_event(person_t p1, person_t p2, epidemic_time_t _time) {
     time = _time;
     affected_people = {p1, p2};
-    preconditions = {std::array<bool,3>({true,false,false}),std::array<bool,3>({false,true,false})};
+    preconditions = {std::array<bool, 3>({true, false, false}),
+                     std::array<bool, 3>({false, true, false})};
     postconditions[0] = I;
   }
 };
@@ -105,14 +111,14 @@ struct recovery_event : public sir_event<1> {
   using sir_event<1>::preconditions;
   using sir_event<1>::postconditions;
   constexpr recovery_event() noexcept {
-    preconditions = {std::array<bool,3>({false,true,false})};
+    preconditions = {std::array<bool, 3>({false, true, false})};
     postconditions[0] = R;
   };
-  recovery_event(const recovery_event&) = default;
+  recovery_event(const recovery_event &) = default;
   recovery_event(person_t p1, epidemic_time_t _time) {
     time = _time;
     affected_people[0] = p1;
-    preconditions = {std::array<bool,3>({false,true,false})};
+    preconditions = {std::array<bool, 3>({false, true, false})};
     postconditions[0] = R;
   }
 };
@@ -123,55 +129,43 @@ struct null_event : public sir_event<0> {
   using sir_event<0>::preconditions;
   using sir_event<0>::postconditions;
   null_event() noexcept = default;
-  null_event(const null_event&) = default;
-  null_event(epidemic_time_t _time) {
-    time = _time;
-  }
+  null_event(const null_event &) = default;
+  null_event(epidemic_time_t _time) { time = _time; }
 };
 
-typedef std::variant < recovery_event, infection_event, null_event > any_sir_event;
+typedef std::variant<recovery_event, infection_event, null_event> any_sir_event;
 constexpr size_t number_of_event_types = 3;
 
-template<size_t index, size_t permutation_index>
+template <size_t index, size_t permutation_index>
 struct sir_event_true_preconditions;
 
-template<>
-struct sir_event_true_preconditions<1,0> {
+template <> struct sir_event_true_preconditions<1, 0> {
   constexpr static auto value = {I};
 };
 
-template<>
-struct sir_event_true_preconditions<2,0> {
+template <> struct sir_event_true_preconditions<2, 0> {
   constexpr static auto value = {S};
 };
 
-template<>
-struct sir_event_true_preconditions<2,1> {
+template <> struct sir_event_true_preconditions<2, 1> {
   constexpr static auto value = {I};
 };
 
-/*******************************************************************************
- * Helper functions for any_sir_event variant type                             *
- *******************************************************************************/
+template <size_t event_index> struct sir_event_by_index;
 
-template<size_t event_index>
-struct sir_event_by_index;
+template <> struct sir_event_by_index<0> : null_event {};
 
-template<>
-struct sir_event_by_index<0> : null_event{};
+template <> struct sir_event_by_index<1> : recovery_event {};
 
-template<>
-struct sir_event_by_index<1> : recovery_event{};
+template <> struct sir_event_by_index<2> : infection_event {};
 
-template<>
-struct sir_event_by_index<2> : infection_event{};
-
-template<size_t N = number_of_event_types, typename = std::make_index_sequence<N> >
+template <size_t N = number_of_event_types,
+          typename = std::make_index_sequence<N>>
 struct sir_event_constructor;
 
-template<size_t N, size_t... indices>
-struct sir_event_constructor<N, std::index_sequence<indices...> > {
-  any_sir_event construct_sir_event(size_t event_index){
+template <size_t N, size_t... indices>
+struct sir_event_constructor<N, std::index_sequence<indices...>> {
+  any_sir_event construct_sir_event(size_t event_index) {
     any_sir_event rc;
     bool any_possible = (false | ... | (event_index == indices));
     if (!any_possible) {
@@ -183,80 +177,62 @@ struct sir_event_constructor<N, std::index_sequence<indices...> > {
   };
 };
 
-template<size_t event_index>
-struct event_size_by_event_index{
-  const static size_t value = sir_event_by_index<event_index>().affected_people.size();
+/*******************************************************************************
+ * Helper functions for any_sir_event variant type                             *
+ *******************************************************************************/
+
+template <size_t event_index> struct event_size_by_event_index {
+  const static size_t value =
+      sir_event_by_index<event_index>().affected_people.size();
 };
 
 struct any_sir_event_size {
-  constexpr size_t operator()(const auto& x){
-
-    return(x.affected_people.size());
-  }
-  /*
-    size_t operator()(const sir_event<0>&x){
-    return(0);
-    }
-    size_t operator()(const sir_event<1>&x){
-    return(1);
-    }
-    size_t operator()(const sir_event<2>&x){
-    return(2);
-    }
-  */
+  size_t operator()(const auto &x) { return (x.affected_people.size()); }
 };
 
 struct any_sir_event_time {
-  epidemic_time_t operator()(const auto&x){
-    return(x.time);
-  }
+  epidemic_time_t operator()(const auto &x) { return (x.time); }
 };
 
 struct any_sir_event_preconditions {
-  person_t& person;
-  auto operator()(const auto&x){
-    return(x.preconditions[person]);
-  }
+  person_t &person;
+  auto operator()(const auto &x) { return (x.preconditions[person]); }
 };
 
 struct any_sir_event_affected_people {
-  person_t& person;
-  auto operator()(const auto&x){
-    return(x.affected_people[person]);
-  }
+  person_t &person;
+  auto operator()(const auto &x) { return (x.affected_people[person]); }
 };
 
 struct any_sir_event_postconditions {
-  person_t& person;
-  auto operator()(const auto&x){
-    return(x.postconditions[person]);
-  }
+  person_t &person;
+  auto operator()(const auto &x) { return (x.postconditions[person]); }
 };
 
 struct any_sir_event_set_time {
-  epidemic_time_t& value;
-  void operator()(auto&x){
+  epidemic_time_t &value;
+  void operator()(auto &x) {
     x.time = value;
     return;
   }
 };
 
-struct any_sir_event_set_affected_people{
-  person_t& position;
-  person_t& value;
-  void operator()(auto&x){
+struct any_sir_event_set_affected_people {
+  person_t &position;
+  person_t &value;
+  void operator()(auto &x) {
     x.affected_people[position] = value;
     return;
   }
 };
 
 struct any_sir_event_print {
-  std::string& prefix;
-  void operator()(const auto&x){
+  std::string &prefix;
+  void operator()(const auto &x) {
     std::cout << prefix;
     std::cout << "time " << x.time << " ";
     std::cout << "size " << x.affected_people.size() << " ";
-    for(auto it : x.affected_people){
+    for (auto it : x.affected_people) {
       std::cout << it << ", ";
     }
     std::cout << std::endl;
@@ -266,33 +242,38 @@ struct any_sir_event_print {
 /*
  * Printing helpers
  */
-void print(const any_sir_event& event, std::string prefix = ""){
-  std::visit(any_sir_event_print{prefix},event);
+void print(const any_sir_event &event, std::string prefix = "") {
+  std::visit(any_sir_event_print{prefix}, event);
 };
 
-constexpr size_t int_pow(size_t base, size_t exponent){
-  if(exponent == 0){return 1;}
-  if(exponent == 1){return base;}
-  size_t sqrtfloor = int_pow(base,exponent / 2);
-  if((exponent % 2) == 0){
-    return(sqrtfloor * sqrtfloor);
+constexpr size_t int_pow(size_t base, size_t exponent) {
+  if (exponent == 0) {
+    return 1;
   }
-  return(sqrtfloor * sqrtfloor * base);
+  if (exponent == 1) {
+    return base;
+  }
+  size_t sqrtfloor = int_pow(base, exponent / 2);
+  if ((exponent % 2) == 0) {
+    return (sqrtfloor * sqrtfloor);
+  }
+  return (sqrtfloor * sqrtfloor * base);
 }
 
-void print(const sir_state& state, std::string prefix = "", bool aggregate = true){
+void print(const sir_state &state, std::string prefix = "",
+           bool aggregate = true) {
   std::cout << prefix << "Possible states at time " << state.time << std::endl;
   int person_counter = 0;
   int state_count = ncompartments;
-  if(!aggregate){
-    for(auto possible_states : state.potential_states){
+  if (!aggregate) {
+    for (auto possible_states : state.potential_states) {
       std::cout << prefix << person_counter << "(";
       state_count = 0;
-      for(auto state : possible_states){
-	if(state){
-	  std::cout << " " << state_count;
-	}
-	++state_count;
+      for (auto state : possible_states) {
+        if (state) {
+          std::cout << " " << state_count;
+        }
+        ++state_count;
       }
       std::cout << " )" << std::endl;
       ++person_counter;
@@ -300,48 +281,31 @@ void print(const sir_state& state, std::string prefix = "", bool aggregate = tru
     return;
   }
 
-  std::array<size_t,int_pow(2,ncompartments)> aggregates;
-  for(size_t subset = 0 ; subset <= int_pow(2,ncompartments); ++subset){
+  std::array<size_t, int_pow(2, ncompartments)> aggregates;
+  for (size_t subset = 0; subset <= int_pow(2, ncompartments); ++subset) {
     aggregates[subset] = 0;
   }
-  for(auto possible_states : state.potential_states){
+  for (auto possible_states : state.potential_states) {
     size_t index = 0;
-    for(size_t state_index = 0; state_index < ncompartments; ++state_index){
-      if(possible_states[state_index]){
-	index += int_pow(2,state_index);
+    for (size_t state_index = 0; state_index < ncompartments; ++state_index) {
+      if (possible_states[state_index]) {
+        index += int_pow(2, state_index);
       }
     }
     aggregates[index]++;
   }
-  for(size_t subset = 0 ; subset <= int_pow(2,ncompartments); ++subset){
-    if(aggregates[subset] > 0){
+  for (size_t subset = 0; subset <= int_pow(2, ncompartments); ++subset) {
+    if (aggregates[subset] > 0) {
       std::cout << prefix;
-      for(size_t compartment = 0; compartment < ncompartments; ++compartment){
-	if((subset%int_pow(2,compartment+1) / int_pow(2,compartment)) == 1){
-	  std::cout << compartment << " ";
-	}
+      for (size_t compartment = 0; compartment < ncompartments; ++compartment) {
+        if ((subset % int_pow(2, compartment + 1) / int_pow(2, compartment)) ==
+            1) {
+          std::cout << compartment << " ";
+        }
       }
       std::cout << " : " << aggregates[subset] << std::endl;
     }
   }
-
 };
-
-auto update_from_descendent = [](sir_state& ours, const sir_state& theirs){
-  if(theirs.time > ours.time){
-    ours.time = theirs.time;
-    for(person_t person = 0 ; person < ours.population_size; ++person){
-      for(auto compartment = 0; compartment < ncompartments; ++compartment){
-	ours.potential_states[person][compartment] = false;
-      }
-    }
-  }
-  for(person_t person = 0 ; person < ours.population_size; ++person){
-    for(auto compartment = 0; compartment < ncompartments; ++compartment){
-      ours.potential_states[person][compartment] = ours.potential_states[person][compartment] || theirs.potential_states[person][compartment];
-    }
-  }
-
- };
 
 #endif
--- test/test_ranges.cpp
+++ test/test_ranges.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 #include <iostream>
 #include <range/v3/all.hpp>
+
+#include <chrono>
+
 // #include <cppcoro/generator.hpp>
 #include "/home/jkaminsky/git/cfepi/take_2/cfepi/include/sir.h"
 
@@ -9,6 +12,24 @@ constexpr void empty_function(auto... x) {};
 
 template<typename T,size_t i>
 using repeat = T;
+
+void update_from_descendent(sir_state &ours, const sir_state &theirs) {
+  if (theirs.time > ours.time) {
+    ours.time = theirs.time;
+    for (person_t person = 0; person < ours.population_size; ++person) {
+      for (size_t compartment = 0; compartment < ncompartments; ++compartment) {
+        ours.potential_states[person][compartment] = false;
+      }
+    }
+  }
+  for (person_t person = 0; person < ours.population_size; ++person) {
+    for (size_t compartment = 0; compartment < ncompartments; ++compartment) {
+      ours.potential_states[person][compartment] =
+          ours.potential_states[person][compartment] ||
+          theirs.potential_states[person][compartment];
+    }
+  }
+};
 
 template<size_t event_index, size_t precondition_index>
 bool check_event_precondition(std::array<bool, number_of_event_types> x){
@@ -25,7 +46,7 @@ template<
   size_t precondition_index
   >
 struct filter_population_by_event_preconditions{
-  static auto get_filtered_reference_vectors(auto && single_gen){
+  static auto get_filtered_reference_vectors(const auto & single_gen){
     auto vector_start = ranges::begin(single_gen);
 
     return(ranges::to<std::vector<size_t> >(
@@ -37,6 +58,13 @@ struct filter_population_by_event_preconditions{
   }
 };
 
+template<size_t event_index>
+const auto transform_lambda = [](auto&& x){
+  sir_event_by_index<event_index> rc;
+  std::apply([&rc](auto... y){rc.affected_people = {y...};}, x);
+  return((any_sir_event) rc);
+ };
+
 template<size_t event_index, typename = std::make_index_sequence<event_size_by_event_index<event_index>::value > >
 struct cartesian_multiproduct;
 
@@ -44,16 +72,11 @@ template<size_t event_index, size_t... precondition_index>
 struct cartesian_multiproduct<event_index,std::index_sequence<precondition_index...> >{
   std::tuple<repeat<std::vector<size_t>, event_size_by_event_index<precondition_index>::value>... > vectors;
   constexpr const static auto apply_lambda = [](auto&... x){return(ranges::views::cartesian_product(x...));};
-  constexpr const static auto transform_lambda = [](auto&& x){
-    sir_event_by_index<event_index> rc;
-    std::apply([&rc](auto... y){rc.affected_people = {y...};}, x);
-    return((any_sir_event) rc);
-  };
   auto cartesian_range(){
     return(std::apply(apply_lambda, vectors));
   };
   auto event_range(){
-    return(ranges::transform_view(cartesian_range(), transform_lambda));
+    return(ranges::transform_view(cartesian_range(), transform_lambda<event_index>));
   };
   // cartesian_multiproduct(std::tuple<repeat<std::vector<size_t>, event_size_by_event_index<precondition_index>::value>... > _vectors) : vectors(std::move(_vectors)) {};
   // cartesian_multiproduct(const cartesian_multiproduct& _from) : vectors(std::move(_from.vectors)) {};
@@ -64,18 +87,28 @@ struct cartesian_multiproduct<event_index,std::index_sequence<precondition_index
   }
 };
 
-template<size_t N = number_of_event_types, typename = std::make_index_sequence<N> >
+template<typename T, size_t N = number_of_event_types, typename = std::make_index_sequence<N> >
+struct any_event_type_range;
+
+template<typename T, size_t N, size_t... event_index>
+struct any_event_type_range<T, N, std::index_sequence<event_index...> > :
+  std::variant<decltype(cartesian_multiproduct<event_index>().event_range())...> {};
+
+template <size_t N = number_of_event_types,
+          typename = std::make_index_sequence<N>>
 struct single_time_event_generator;
 
-template<size_t N, size_t... event_index>
-struct single_time_event_generator<N, std::index_sequence<event_index...> > {
+template <size_t N, size_t... event_index>
+struct single_time_event_generator<N, std::index_sequence<event_index...>> {
 
   std::tuple<cartesian_multiproduct<event_index>...> product_thing;
 
-  constexpr const static auto concat_lambda = [](auto&... x) {
-    return(ranges::views::concat(x.event_range()...));
+  constexpr const static auto concat_lambda = [](auto &...x) {
+    return (ranges::views::concat(x.event_range()...));
   };
 
+  std::variant<decltype(std::get<event_index>(product_thing).event_range())...>
+      range_variant;
   /*
     cppcoro::generator<any_sir_event> test(){
     for(any_sir_event elem : std::get<0>(product_thing).event_range()){
@@ -84,51 +117,85 @@ struct single_time_event_generator<N, std::index_sequence<event_index...> > {
     }
   */
 
-  auto event_range() {
-    return(std::apply(concat_lambda,product_thing));
-  };
+  auto event_range() { return (std::apply(concat_lambda, product_thing)); };
 
   single_time_event_generator() = default;
-  single_time_event_generator(sir_state& initial_conditions) :
-    product_thing(cartesian_multiproduct<event_index>(initial_conditions)...) {
-  };
+  single_time_event_generator(sir_state &initial_conditions)
+      : product_thing(
+            cartesian_multiproduct<event_index>(initial_conditions)...){};
 };
 
-template<size_t event_index>
-void main_func(sir_state& initial_conditions){
+template <size_t event_index>
+size_t iterate_through(sir_state &initial_conditions) {
   size_t counter = 0;
-  single_time_event_generator<number_of_event_types> gen2(initial_conditions);
-  // single_time_event_generator<number_of_event_types> gen2;
   cartesian_multiproduct<event_index> gen(initial_conditions);
-  auto event_range = gen2.event_range();
-  // auto event_range = gen.event_range();
-  for(auto elem : event_range){
+  auto event_range = gen.event_range();
+
+  for (auto elem : event_range) {
     ++counter;
   }
-  // std::cout << "finished iterating\n";
-  // std::cout << "counter is " << counter << "\n";
+  return (counter);
 };
 
-int main(int argc, char** argv){
+size_t iterate_through(sir_state &initial_conditions) {
+  size_t counter = 0;
+  single_time_event_generator<number_of_event_types> gen(initial_conditions);
+  auto event_range = gen.event_range();
+  for (auto elem : event_range) {
+    ++counter;
+  }
+  return (counter);
+};
+
+int main(int argc, char **argv) {
 
   if (argc != 3) {
-    std::cout << "This program takes 2 runtimes arguments:\n\t- The number of people in the population\n\t- The number of time steps to run over\n";
+    std::cout
+        << "This program takes 2 runtimes arguments:\n\t- The number of people "
+           "in the population\n\t- The number of time steps to run over\n";
     return -1;
   }
   size_t state_size = atoi(argv[1]);
   epidemic_time_t epidemic_length = atoi(argv[2]);
-  std::cout << "Running " << epidemic_length << " time steps on a population of size " << state_size << "\n";
+  std::cout << "Running " << epidemic_length
+            << " time steps on a population of size " << state_size << "\n";
 
   std::string prefix = "main : ";
   sir_state initial_conditions = default_state(state_size);
-  for(size_t elem : ranges::views::iota(0,epidemic_length)){
-    if (elem < state_size){
+  decltype(std::chrono::steady_clock::now() -
+           std::chrono::steady_clock::now()) b1_prev_elapsed;
+  decltype(std::chrono::steady_clock::now() -
+           std::chrono::steady_clock::now()) b2_prev_elapsed;
+  for (size_t elem : ranges::views::iota(0, epidemic_length)) {
+    if (elem < state_size) {
       initial_conditions.potential_states[elem][I] = true;
     }
-    // main_func<0>(initial_conditions);
-    // main_func<1>(initial_conditions);
-    main_func<2>(initial_conditions);
 
-    // main_func(initial_conditions);
+    auto b2_start = std::chrono::steady_clock::now();
+    iterate_through(initial_conditions);
+    auto b2_end = std::chrono::steady_clock::now();
+    auto b2_elapsed = b2_end - b2_start;
+
+    auto b1_start = std::chrono::steady_clock::now();
+    iterate_through<0>(initial_conditions);
+    iterate_through<1>(initial_conditions);
+    iterate_through<2>(initial_conditions);
+    auto b1_end = std::chrono::steady_clock::now();
+    auto b1_elapsed = b1_end - b1_start;
+
+    std::cout << "Unified : " << b2_elapsed.count() << "\n"
+              << "Separate : " << b1_elapsed.count() << "\n"
+              << "Ratio : " << b1_elapsed.count() * 1. / b2_elapsed.count()
+              << "\n"
+              << "Unified Diff : "
+              << b2_elapsed.count() - b2_prev_elapsed.count() << "\n"
+              << "Separate Diff : "
+              << b1_elapsed.count() - b1_prev_elapsed.count() << "\n"
+              << "Diff Ratio : "
+              << (b1_elapsed.count() - b1_prev_elapsed.count()) * 1. /
+                     (b2_elapsed.count() - b2_prev_elapsed.count())
+              << "\n";
+    b1_prev_elapsed = b1_elapsed;
+    b2_prev_elapsed = b2_elapsed;
   }
 }
